e592ef812f2d1c8d47db1db11cd895145fc54676
yamskoo <yamine.kasko@edu.devinci.fr>
2025-10-06 20:35:35 +0200

    analys-google

---
+++ assets/ga-init.js
@@ -0,0 +1,9 @@
+// /assets/ga-init.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+
+// Basic page view
+gtag('config', 'G-7QV52QBWLT', {
+  anonymize_ip: true    // (good practice in EU)
+});
